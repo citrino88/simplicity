ae15d92a4f02ceef1fc91947bed51c5ffca57d58
citrino88 <almeida.citrino@gmail.com>
2024-04-30 16:24:37 -0300

    menu funcional JS abre e fecha

--- js/menu.js
+++ js/menu.js
@@ -4,14 +4,22 @@ console.log("menu");
 
 /* Selecionar o elemento que acionará a lista de links do menu  */
 
-const botaoMenu = document.querySelector("nav h2");
+const botaoMenu = document.querySelector("nav h2 a");
 // esse é o elemento que vai selecionar a lista do menu
 
 /* Selecionando a lista de links / menu */
 const listaDeLinks = document.querySelector(".links-menu");
 
 /* Monitorando o evento de click/toque no botaoMenu */
-botaoMenu.addEventListener("click", function name(params) {
+botaoMenu.addEventListener("click", function name(event) {
+    /* "Prevenir / anular" o evento padrão do link de redirecionamento/recarregamento da página */
+    event.preventDefault();
     console.log("belezzza!");
-    
+    listaDeLinks.classList.toggle("aberto") // para ter a alternância
+
+    if(listaDeLinks.classList.contains ("aberto")){
+        botaoMenu.innerHTML = "Fechar &times;"
+    }else{
+        botaoMenu.innerHTML = "Menu &equiv;"
+    }
 });
