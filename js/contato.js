5ea237ac43e2a7309a8283a348f42995cb332f36
citrino88 <almeida.citrino@gmail.com>
2024-05-02 15:29:13 -0300

    event.preventDefault

--- js/contato.js
+++ js/contato.js
@@ -14,3 +14,10 @@ const botaoBuscar = formulario.querySelector("#buscar");
 const mensagem = formulario.querySelector("#status");
 /* obs: formulario.querySelector(""); AO INVÉS DE document.querySelector("#estado"); pois só procura dentro do formulario, melhora a perforce, não precisa percorrer o documento inteiro. */
 
+/* Detectando quando o botão de buscar CEP é acionado */
+//addEventListener = acionar um Ouvidor de Evento (quando ocorrer o Evento tal)
+botaoBuscar.addEventListener("click", function(event){
+    console.log("agora vai!");
+    /* Anular o comportamento padrão de redirecionamento/recarregamento da página. Sempre aconte ao trabalhar com <a> e <form> */
+    event.preventDefault();
+});
