beb4b2da5d8c04bfd909c59cb49ed7a4699affd1
citrino88 <almeida.citrino@gmail.com>
2024-05-02 16:47:11 -0300

    verificacao do CEP JS

--- js/contato.js
+++ js/contato.js
@@ -11,13 +11,44 @@ const campoBairro = formulario.querySelector("#bairro");
 const campoCidade = formulario.querySelector("#cidade");
 const campoEstado = formulario.querySelector("#estado");
 const botaoBuscar = formulario.querySelector("#buscar");
-const mensagem = formulario.querySelector("#status");
+const mensagemStatus = formulario.querySelector("#status");
 /* obs: formulario.querySelector(""); AO INVÉS DE document.querySelector("#estado"); pois só procura dentro do formulario, melhora a perforce, não precisa percorrer o documento inteiro. */
 
 /* Detectando quando o botão de buscar CEP é acionado */
 //addEventListener = acionar um Ouvidor de Evento (quando ocorrer o Evento tal)
 botaoBuscar.addEventListener("click", function(event){
-    console.log("agora vai!");
+    //console.log("agora vai!");
     /* Anular o comportamento padrão de redirecionamento/recarregamento da página. Sempre aconte ao trabalhar com <a> e <form> */
     event.preventDefault();
+
+    /* Verificando se o cep NÃO TEM 8 dígitos */
+    if ( campoCep.value.length !== 8){
+        /* Informar o usuário sobre o erro: */
+        mensagemStatus.textContent = "Digite um CEP válido";
+        mensagemStatus.style.color = "purple";
+        /* Parar completamente a execução */
+        return;
+    /* }else{
+        guardar o valor do cep digitado
+        let cepInformado = campoCep.value; */
+    }
+
+    /* Guardando o valor do cep digitado/informado, abaixo sem o else, é chamado code clean (também, early return) */
+    let cepInformado = campoCep.value;
+    //console.log(cepInformado); para testar se está funcionando
+
+    /* AJAX - Asyncronous JavaScript And XML
+    (JavaScript assíncrono e XML) 
+    
+    Técnica de comunicação (transmissão, recebimento) de dados que permite o processamento em conjunto com APIs (ou Web Services)
+    */
+
+    // Etapa 1: prepara a URL da API com o CEP informado
+
+    // Etapa 2: acessar a API (com a URL) e guardar o retorno dela
+
+    // Etapa 3: extrair os dados da resposta da API em formado JSON
+
+    // Etapa 4: lidar com os dados (em caso de erro e de sucesso)
 });
+
