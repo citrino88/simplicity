0ae680e2c569e19ae92a746a1dad04246fa1b6b8
citrino88 <almeida.citrino@gmail.com>
2024-05-09 14:23:59 -0300

    configurando mascara para cep e telefone

--- js/contato.js
+++ js/contato.js
@@ -10,9 +10,14 @@ const campoEndereco = formulario.querySelector("#endereco");
 const campoBairro = formulario.querySelector("#bairro");
 const campoCidade = formulario.querySelector("#cidade");
 const campoEstado = formulario.querySelector("#estado");
+const campoTelefone = formulario.querySelector("#telefone");
 const botaoBuscar = formulario.querySelector("#buscar");
 const mensagemStatus = formulario.querySelector("#status");
 /* obs: formulario.querySelector(""); AO INVÉS DE document.querySelector("#estado"); pois só procura dentro do formulario, melhora a perforce, não precisa percorrer o documento inteiro. */
+
+/* Seleção dos campos e ativação das máscaras */
+$(campoCep).mask("00000-000"); //01234-567 (a máscara segura, e usuario só consegue digitar conforme especificado a esquerda aí)
+$(campoTelefone).mask("(00) 0000-0000"); // (11) 2135-0300
 
 /* Detectando quando o botão de buscar CEP é acionado */
 //addEventListener = acionar um Ouvidor de Evento (quando ocorrer o Evento tal)
@@ -21,8 +26,8 @@ botaoBuscar.addEventListener("click", async function(event){
     /* Anular o comportamento padrão de redirecionamento/recarregamento da página. Sempre aconte ao trabalhar com <a> e <form> */
     event.preventDefault();
 
-    /* Verificando se o cep NÃO TEM 8 dígitos */
-    if ( campoCep.value.length !== 8){
+    /* Verificando se o cep NÃO TEM 9 dígitos */
+    if ( campoCep.value.length !== 9){
         /* Informar o usuário sobre o erro: */
         mensagemStatus.textContent = "Digite um CEP válido";
         mensagemStatus.style.color = "purple";
